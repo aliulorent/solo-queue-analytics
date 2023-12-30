6b3085fa5129d8c5f619332cd494dc5e034618b4
aliulorent <aliulorent@gmail.com>
2023-12-30 16:46:46 -0500

    Converted server from commonjs -> esmodule

--- server/app.js
+++ server/app.js
@@ -1,16 +1,27 @@
-require('dotenv').config();
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+import dotenv from "dotenv";
+import axios from "axios";
+import express from "express";
+import mysql from "mysql2/promise";
 
-const mysql = require('mysql2')
-const connection = mysql.createConnection(process.env.DATABASE_URL);
+dotenv.config();
+const app = express();
 
-connection.connect()
+const port = process.env.PORT || 3000;
+const riotKey = process.env.RIOT_KEY;
+
+const connection = await mysql.createConnection(process.env.DATABASE_URL);
+
+// connection.connect();
 
 app.get('/', (req, res) => {
     res.send("Welcome to my API!");
 })
+
+app.get('/getSummoner/:region/:name/:tag', async (req, res) => {
+    const query = `SELECT * FROM users WHERE riot_name = ? AND tag_line = ? AND region = ?`;
+    const [rows] = await connection.query(query, [req.params.name, req.params.tag, req.params.region]);
+    res.send(rows);
+});
 
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`);
