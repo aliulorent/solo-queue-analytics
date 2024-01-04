9e070e6b6df3ed3506ad190acffaeb9564566641
aliulorent <aliulorent@gmail.com>
2024-01-04 14:40:20 -0500

    Minor styling tweaks

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,7 +3,7 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <main class="bg-blue-900">
+  <main class="bg-blue-900 min-h-screen">
     <RouterView />
   </main>
 </template>
--- client/src/views/HomeVIew.vue
+++ client/src/views/HomeVIew.vue
@@ -2,7 +2,7 @@
 import SearchBar from "../components/SearchBar.vue"
 </script>
 <template>
-    <div class="flex justify-center items-center bg-blue-500">
+    <div class="flex justify-center items-center min-h-screen">
         <SearchBar />
     </div>
 </template>
