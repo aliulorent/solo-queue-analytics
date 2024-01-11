3534b6e4cca629f4b79e8f2b0b458a2ea729008a
aliulorent <aliulorent@gmail.com>
2024-01-11 17:11:02 -0500

    Changed favicon & titles

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import {createRouter, createWebHistory, useRoute} from 'vue-router'
 import HomeView from "../views/HomeView.vue"
 import SummonerView from "../views/SummonerView.vue"
+import { nextTick } from 'vue'
 
 
 const router = createRouter({
@@ -9,12 +10,26 @@ const router = createRouter({
         {
             path: "/",
             component: HomeView,
+            name: "home",
+            meta: {title: "Solo Queue Analytics"}
         },
         {
             path: "/summoner/:region/:name/:tag",
+            name: "summoner",
             component: SummonerView,
+            meta: {title: "S"}
         }
     ]
 })
+
+router.afterEach((to, from) => {
+    const route = useRoute();
+    if(to.name==="summoner"){
+         const {name, tag} = route.params;
+        nextTick(() => {
+            document.title = `${name} #${tag}` || "Solo Queue Analytics";
+        });
+    }  
+});
 
 export default router;
