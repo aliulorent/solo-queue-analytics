5f3c3a7ae4a71d1173bf82db17e4da39cc7cdfff
aliulorent <aliulorent@gmail.com>
2024-01-22 15:02:24 -0500

    Added basic header text to home view

--- client/src/views/HomeVIew.vue
+++ client/src/views/HomeVIew.vue
@@ -2,7 +2,11 @@
 import SearchBar from "../components/SearchBar.vue"
 </script>
 <template>
-    <div class="flex justify-center items-center min-h-screen">
+    <div class="flex flex-col justify-center items-center gap-4 min-h-screen">
+        <div>
+            <!-- Title Container -->
+            <h1 class="text-5xl font-bold -mt-20">Solo <span class="text-secondary">Queue </span> Analytics</h1>
+        </div>
         <SearchBar />
     </div>
 </template>
